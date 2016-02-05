ae6b4757443b282e75f347aad26520ffb344bc78
Farhadur Rahim <varendrasoft@gmail.com>
2016-02-05 06:45:46 +0600

    Create jQuery.Coud.js

---
+++ jQuery.Coud.js
@@ -0,0 +1,70 @@
+/* plgName       : jQuery.Cloud.js
+ * Licence       : MIT licensed - Copyright (C) 2015-2016 Farhadur Rahim @webmechanicx.
+ * Released      : 02/07/2016
+ * Version       : jQuery 
+ * Description   : Zero Configuration, Only Dependency is jQuery and compatible with every version.
+ * Usage:        : Please visit github page - cloudjs.webmechanicx.github.io,
+ * repositoy     : 
+ */
+
+(function($) {
+    $.fn.Cloud = function( options ) {
+        
+        var elem             = $( this ),
+            getButtons       = '',
+            cloudButtons     = '',
+            siteTitle        = $(document).attr("title"),
+            uriBase          = window.location.protocol + '//' + window.location.host,
+            onlyfilename     = '';
+           
+        /*establish our default settings for Cloud APIs*/
+        /*@settings Google-Drive | Dropbox APIs and other if you wise to add*/
+        
+        var settings = $.extend({
+            googleDrive  : 'auto', 
+            dropBox      : 'a1jy330vpyki64r'
+        }, options);
+       
+        return this.each( function() {
+
+                   uriBase        =  uriBase + $(this).data("file-link");
+                   onlyfilename     = uriBase.match(/.*\/([^/]+)\.([^?]+)/i)[1]; //return only a valid file
+            
+                   getButtons = getBtnHtml(settings, onlyfilename, uriBase);  /*get buttons chunk*/
+                   $(this).append(getButtons); 
+           
+        }).promise().then(function(){   /*@call jQuery.promise for immediate invocation*/
+                   
+			if('dropBox' in settings){
+                    var dropApi = document.createElement('script');
+                    dropApi.setAttribute("type","text/javascript");
+                    dropApi.setAttribute("src", 'https://www.dropbox.com/static/api/2/dropins.js');
+                    dropApi.setAttribute("id", "dropboxjs");
+                    dropApi.setAttribute("data-app-key", settings.dropBox);
+                    document.getElementsByTagName("head")[0].appendChild(dropApi);
+            }
+									 
+			if('googleDrive' in settings){
+                    var googleApi = document.createElement('script');
+                    googleApi.setAttribute("src", 'https://apis.google.com/js/platform.js');
+                    googleApi.setAttribute("async", true);
+                    googleApi.setAttribute("defer", true);
+                    document.getElementsByTagName("head")[0].appendChild(googleApi);
+            }
+            
+        });
+        
+       function getBtnHtml(settings, onlyfilename, uriBase){
+
+                if('googleDrive' in settings){
+			        cloudButtons += '<a class="g-savetodrive" data-src="' + uriBase + '" data-filename="' + onlyfilename + '" data-sitename="' + siteTitle + '"></a>';	
+    		    }    
+		        if('dropBox' in settings){
+			       cloudButtons += '<a href="' + uriBase + '" class="dropbox-saver"></a>';
+		          }
+           
+		    return cloudButtons;
+        }
+    }
+    
+}(jQuery));
